1fcedb33c94ed08ad65d0cd5eb94575868227d69
Weijia Jiang <jiangwjia@163.com>
2020-11-02 17:09:55 +0800

    [12] 修改脚本文件

--- updateBlob.sh
+++ updateBlob.sh
@@ -1,5 +1,12 @@
 if [ ! -d "/root/blob/public/" ];then
 echo "文件夹不存在"
 else
-rm -rf /root/blob/public
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 开始更新仓库。"
+git pull origin main
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 仓库更新完毕，开始编译环境。"
+
+hexo clean && hexo g
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> OK "
 fi
